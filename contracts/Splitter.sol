86d21269cce7d4cbd9b6fb865d473aa2246d5185
ConstantinKloecker <github@kloecker.io>
2019-04-09 16:06:50 +0200

    Updated contract to V2

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,49 +2,50 @@ pragma solidity ^0.5.0;
 
 contract Splitter {
 
+    bool active;
+    address owner;
     mapping (address => uint) balances;
-    address alice;
-    address bob;
-    address carol;
 
-    event LogEthSplitted(address indexed _from, uint _amount);
+    event LogEthSplitted(address indexed _from, uint _amount, address _toUser1, address _toUser2);
+    event LogRemainder(address indexed _user);
     event LogWithdrawal(address indexed _to, uint _amount);
 
-    constructor(address _alice, address _bob, address _carol) public {
-        alice = _alice;
-        bob = _bob;
-        carol = _carol;
+    constructor() public {
+        owner = msg.sender;
+        active = true;
     }
 
-    function getSenderBalance() public view returns (uint) {
-        return balances[msg.sender];
+    function deactivateSplitter() public {
+        require(msg.sender == owner, "Only executable by owner");
+        active = false;
     }
 
-    function getAllBalances() public view returns (uint, uint) {
-        return (balances[bob], balances[carol]);
+    function getBalance(address _user) public view returns (uint) {
+        return balances[_user];
     }
 
     function withdraw() public {
-        uint amount = getSenderBalance();
-        if (amount > 0) {
-            balances[msg.sender] = 0;
-            if (!msg.sender.send(amount)) {
-                revert("Error during withdrawal");
-            }
-            emit LogWithdrawal(msg.sender, amount);
-        } else {
-            revert("No balance available");
+        uint amount = getBalance(msg.sender);
+        require(amount > 0, "No balance available");
+        balances[msg.sender] = 0;
+        require(msg.sender.send(amount) == true, "Error during withdrawal");
+        emit LogWithdrawal(msg.sender, amount);
+    }
+
+    function splitEth(address _toUser1, address _toUser2) external payable {
+        require(active == true, "Contract is no longer active");
+        uint amount = msg.value / 2;
+        balances[_toUser1] += amount;
+        balances[_toUser2] += amount;
+        emit LogEthSplitted(msg.sender, msg.value, _toUser1, _toUser2);
+        uint remainder = msg.value - (2 * amount);
+        if (remainder != 0) {
+            balances[msg.sender] += remainder;
+            emit LogRemainder(msg.sender);
         }
     }
 
-    function() external payable {
-        if (msg.sender == alice) {
-            uint amount = msg.value / 2;
-            balances[bob] += amount;
-            balances[carol] += amount;
-            emit LogEthSplitted(msg.sender, msg.value);
-        } else {
-            revert("Only usable by Alice");
-        }
+    function() external {
+        revert("Please use functions");
     }
 }
