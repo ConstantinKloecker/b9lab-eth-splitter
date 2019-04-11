d4d93a3295ec141da341ab50334e795a8d6e96e8
ConstantinKloecker <github@kloecker.io>
2019-04-11 08:32:09 +0200

    Added function to change owner

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -4,6 +4,8 @@ contract Owned {
 
     address private owner;
 
+    event LogOwnerChanged(address newOwner);
+
     constructor() internal {
         owner = msg.sender;
     }
@@ -11,5 +13,10 @@ contract Owned {
     modifier onlyOwner() {
         require(msg.sender == owner, "Only executable by owner");
         _;
+    }
+
+    function changeOwner(address newOwner) public onlyOwner {
+        owner = newOwner;
+        emit LogOwnerChanged(newOwner);
     }
 }
